48bed877b34bd047dbb085dfc128e4776fdb7303
aidengaul <100671205+aidengaul@users.noreply.github.com>
2023-04-21 16:14:39 -0400

    Update HashTable.cpp

--- src/HashTable.cpp
+++ src/HashTable.cpp
@@ -1,46 +1,75 @@
-#include <fstream>
-#include <sstream>
-#include <chrono>
-#include "Person.h"
 #include "HashTable.h"
 
-void ParseFile(std::string fileName, HashTable& table);
-// do smth to read command line instructions
-
-int main() {
-    HashTable table;
-    ParseFile("data/data.txt", table);
-
-    return 0;
+HashTable::HashTable() {
+    numBuckets = 1000;
+    numEntries = 0;
+    hTable = new std::vector<Person>[numBuckets];
+    loadFactor = (float) numEntries / (float) numBuckets;
 }
 
-void ParseFile(std::string fileName, HashTable& table) {
-    std::ifstream inFile(fileName);
-    std::string line;
-    std::string word;
-    std::vector<std::string> row;
-    std::vector<Person> people;
-    int num = 0;
+void HashTable::UpdateLoad() {
+    loadFactor = (float) numEntries / (float) numBuckets;
+}
 
-    auto start = std::chrono::high_resolution_clock::now();
+void HashTable::Insert(std::string fName, std::string lName, std::string address, std::string city, std::string state,
+                       std::string zip, std::string phoneNum) {
+    // check loadFactor, insert accordingly
+    if (loadFactor < 0.8)
+        hTable[Hash(fName)].emplace_back(fName, lName, address, city, state, zip, phoneNum);
+    else {
+        ReHash();
+        hTable[Hash(fName)].emplace_back(fName, lName, address, city, state, zip, phoneNum);
+    }
 
-    if (inFile.is_open()) {
-        while (std::getline(inFile, line)) {
-            row.clear();
-            std::stringstream s(line);
+    numEntries++;
+    UpdateLoad();
+}
 
-            while (std::getline(s, word, ',')) {
-                row.push_back(word);
-            }
+int HashTable::Hash(std::string fName) {
+    // hash function based on https://cp-algorithms.com/string/string-hashing.html#search-for-duplicate-strings-in-an-array-of-strings
+    int p = 29791;
+    long long currentHash = 0;
 
-            // insert into black red tree
-            table.Insert(row[0], row[1], row[2], row[3], row[4], row[5], row[6]);
-            num++;
+    for (int i = 0; i < fName.length(); i++) {
+        currentHash += (fName[i] * (int)std::pow(p, i));
+    }
+
+    // if hash val overflows simply change to positive
+    if (currentHash < 0)
+        currentHash *= -1;
+
+    return (int)(currentHash % numBuckets);
+}
+
+void HashTable::ReHash() {
+    std::vector<Person>* temp = new std::vector<Person>[numBuckets * 2];
+
+    // copy each Person obj from current hTable to temp
+    for (int i = 0; i < numBuckets; i++) {
+        for (int j = 0; j < hTable[i].size(); j++) {
+            temp[Hash(hTable[i][j].fName)].push_back(hTable[i][j]);
         }
     }
 
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = duration_cast<std::chrono::microseconds>(stop - start);
+    // increase numBuckets, assign hTable to temp, and clear temp
+    numBuckets *= 2;
+    hTable = temp;
+    temp->clear();
 
-    std::cout << "Inserted " << num << " elements in " << duration.count() << " microseconds" << std::endl;
+    UpdateLoad();
+}
+
+void HashTable::Search(std::string first, std::string last) {
+    int index = Hash(first);
+
+    if (hTable[index].size() > 0) {
+        for (int i = 0; i < hTable[index].size(); i++) {
+            if (hTable[index][i].lName == last)
+                // do smth here
+                std::cout << "Found";
+        }
+    }
+    else {
+        std::cout << "Not found" << std::endl;
+    }
 }
